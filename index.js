48e80db63e0900e53ea90aaf23e4e42a100bb1bb
Tommy <ayoelx@users.noreply.github.com>
2020-05-10 16:23:01 +0100

    add input validation with JS

---
+++ index.js
@@ -0,0 +1,35 @@
+let userEmail = document.getElementById('userEmail');
+let button = document.getElementById('btn');
+let prompt = undefined;
+
+if (window.innerWidth < 550) {
+    prompt = document.getElementById('invalid-prompt');
+    alert('small');
+}
+
+let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+function validateEmail(email) {
+    if (reg.test(email)) {
+        return true
+    }
+    return false
+}   
+
+function result() {
+    if (validateEmail(userEmail.value)) {
+        if (userEmail.classList.contains('invalid')) {
+            userEmail.classList.remove('invalid');
+            prompt.innerText = '';
+            console.log('Invalid class removed');
+        }
+        //pass
+    } else if (!userEmail.classList.contains('invalid')){
+        userEmail.classList.add('invalid');
+        prompt.innerText = 'Please provide a valid email address';
+        console.log('Invalid class added!')
+    }
+}
+
+userEmail.addEventListener("focusout", result)
+button.addEventListener("click", result)
